cc9cfdb75ef6189de26d600267d77fa2eebfacc6
Chad Woolley <thewoolleyman@gmail.com>
2018-04-12 07:09:55 -0700

    add testing via 'bower i purescript-assert --save'

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,9 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Euler (answer)
+import Test.Assert (assert)
+
 main = do
-  log "You should add some tests."
+  assert (answer == 233168)
